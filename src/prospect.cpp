9ca1ad52785fbd8680952c0e2aa7ab2a9b205038
Mark Harmstone <mark@harmstone.com>
2020-06-26 10:41:42 +0100

    add get_user_settings

--- src/prospect.cpp
+++ src/prospect.cpp
@@ -1,7 +1,50 @@
 #include <stdio.h>
+#include <string>
+#include "xml.h"
+
+using namespace std;
+
+static void get_user_settings(const string& url, const string& mailbox) {
+    xml_writer req;
+
+    req.start_document();
+    req.start_element("soap:Envelope", { { "soap", "http://schemas.xmlsoap.org/soap/envelope/" }, { "a", "http://schemas.microsoft.com/exchange/2010/Autodiscover" }, { "wsa", "http://www.w3.org/2005/08/addressing" } });
+
+    req.start_element("soap:Header");
+    req.element_text("a:RequestedServerVersion", "Exchange2010");
+    req.element_text("wsa:Action", "http://schemas.microsoft.com/exchange/2010/Autodiscover/Autodiscover/GetUserSettings");
+    req.element_text("wsa:To", url);
+    req.end_element();
+
+    req.start_element("soap:Body");
+    req.start_element("a:GetUserSettingsRequestMessage");
+    req.start_element("a:Request");
+
+    req.start_element("a:Users");
+    req.start_element("a:User");
+    req.element_text("a:Mailbox", mailbox);
+    req.end_element();
+    req.end_element();
+
+    req.start_element("a:RequestedSettings");
+    req.element_text("a:Setting", "InternalEwsUrl");
+    req.end_element();
+
+    req.end_element();
+    req.end_element();
+    req.end_element();
+
+    req.end_element();
+    req.end_document();
+
+    printf("%s\n", req.dump().c_str());
+
+    // FIXME - send via libcurl
+    // FIXME - parse response
+}
 
 int main() {
-    printf("Hello, world\n");
+    get_user_settings("https://autodiscover.boltonft.nhs.uk/autodiscover/autodiscover.svc", "mark.harmstone@boltonft.nhs.uk");
 
     return 0;
 }
---
+++ src/xml.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+#include <libxml/xmlwriter.h>
+
+class xml_writer {
+public:
+    xml_writer();
+    ~xml_writer();
+    std::string dump() const;
+    void start_document();
+    void end_document();
+    void start_element(const std::string& tag, const std::unordered_map<std::string, std::string>& namespaces = {});
+    void end_element();
+    void text(const std::string& s);
+
+    void element_text(const std::string& tag, const std::string& s, const std::unordered_map<std::string, std::string>& namespaces = {}) {
+        start_element(tag, namespaces);
+        text(s);
+        end_element();
+    }
+
+    void attribute(const std::string& name, const std::string& value);
+
+private:
+    xmlBufferPtr buf;
+    xmlTextWriterPtr writer;
+};
